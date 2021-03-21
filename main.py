0db829c429443b8f2ad5fbadee7365cbe2671cd8
Sefa <sefaydinelli@hotmail.com>
2021-03-21 18:44:18 +0300

    Endpoints created

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1> Sefa Aydinelli </h1>"
+
+@app.route("/whoami")
+def who_am_i():
+    firstname = request.args.get("firstname")
+    lastname = request.args.get("lastname")
+    return jsonify(firstname = firstname, lastname = lastname)
 
 if __name__ == "__main__":
     app.run()
